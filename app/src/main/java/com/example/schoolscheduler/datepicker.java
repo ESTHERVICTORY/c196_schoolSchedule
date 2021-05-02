7d2430d5bdb63c65baa71c54b8150e9ac020cd6f
estherestherv <32546768+estherestherv@users.noreply.github.com>
2021-05-01 21:08:49 -0400

    First Commit
    Navigational Menu

---
+++ app/src/main/java/com/example/schoolscheduler/datepicker.java
@@ -0,0 +1,16 @@
+package com.example.schoolscheduler;
+
+import android.app.DatePickerDialog;
+
+public class datepicker {
+//    private DatePickerDialog datePickerDialog;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState){
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//    public void openDatePicker(View v){
+//
+//    }
+}
