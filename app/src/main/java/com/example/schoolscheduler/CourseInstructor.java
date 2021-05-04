598fbd75c288452c0090c95580e3d7f47bed5e12
estherestherv <32546768+estherestherv@users.noreply.github.com>
2021-05-04 11:25:24 -0400

    First Commit
    Navigational Menu
    
    Opening Menu Items

---
+++ app/src/main/java/com/example/schoolscheduler/CourseInstructor.java
@@ -0,0 +1,14 @@
+package com.example.schoolscheduler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseInstructor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_instructor);
+    }
+}
--- app/src/main/java/com/example/schoolscheduler/MainActivity.java
+++ app/src/main/java/com/example/schoolscheduler/MainActivity.java
@@ -1,37 +1,76 @@
 package com.example.schoolscheduler;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//Create Toolbar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+//Selecting Menu Items
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
     }
+
     @Override
-    public void onBackPressed (){
-        if (drawer.isDrawerOpen(GravityCompat.START)){
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+
+
+            case R.id.term:
+                Intent intent2 = new Intent(MainActivity.this , Term.class);
+                startActivity(intent2);
+                break;
+
+            case R.id.courses:
+                Intent intent3 = new Intent(MainActivity.this , Courses.class);
+                startActivity(intent3);
+                break;
+
+            case R.id.courseInstructor:
+                Intent intent4 = new Intent(MainActivity.this , CourseInstructor.class);
+                startActivity(intent4);
+                break;
+
+            case R.id.assessments:
+                Intent intent5 = new Intent(MainActivity.this , Courses.class);
+                startActivity(intent5);
+                break;
+        }
+       drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }else{
-           super.onBackPressed();
+        } else {
+            super.onBackPressed();
         }
 
     }
